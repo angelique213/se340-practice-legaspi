45b12b17577935a3fabb32b250d45d22d981b376
angelique213 <angeliquelegaspi123@gmail.com>
2026-04-28 22:09:53 +0800

    Initial commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+const add = (a, b) => a + b;
+console.log(add(2, 3));
+
+const square = number => number * number;
+console.log(square(4));
+
+const name = "Angelique";
+const message = `Hello, my name is ${name}!`;
+console.log(message);
